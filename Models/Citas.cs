2e123d2fe4b9de1099059381bfb8f6c72898f816
ErickVO <59853887+ErickVO@users.noreply.github.com>
2021-10-11 02:44:08 -0400

    Añadidura de Clase y registro

---
+++ Models/Citas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClientCitas.Models
+{
+    public class Citas
+    {
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public int CitaId { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public string Apellidos { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        public DateTime Fecha_Cita { get; set; }
+        public string Username { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,7 +20,7 @@ namespace WebClientCitas
             builder.RootComponents.Add<App>("#app");
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazoredToast();
             builder.Services
                 .AddScoped<IAuthServices, AuthServices>();
 
